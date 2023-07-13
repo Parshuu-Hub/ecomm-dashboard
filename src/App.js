ae8318aa8732edae89fca760403aebe27b51185f
Praveen Mishra <mishrarajan282@gmail.com>
2023-07-13 15:11:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import {Button} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jai Siya Ram</h1>
+      <button>Normal</button>
+      <Button>Bootstrap Btn</Button>
     </div>
   );
 }
 
 export default App;
+
+
+
+//Github UserName : Parshuu-Hub
+//Github Password : #P@rshuu007
